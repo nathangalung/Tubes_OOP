1da65c11c6b731a1e455a32abd0b878710cb5c02
nathangalung <bryanhutagalung246@gmail.com>
2024-04-30 15:37:55 +0700

    update dolphin rider & squash

--- src/plant/Plant.java
+++ src/plant/Plant.java
@@ -1,5 +1,49 @@
 package plant;
 
-public class Plant {
-    
+public abstract class Plant {
+    private String name;
+    private int cost;
+    private int health;
+    private int attack_damage;
+    private int attack_speed;
+    private int range;
+    private int cooldown;
+
+    public Plant(String name, int cost, int health, int attack_damage, int attack_speed, int range, int cooldown) {
+        this.name = name;
+        this.cost = cost;
+        this.health = health;
+        this.attack_damage = attack_damage;
+        this.attack_speed = attack_speed;
+        this.range = range;
+        this.cooldown = cooldown;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }   
+
+    public int getHealth() {
+        return this.health;
+    }   
+
+    public int getAttackDamage() {
+        return this.attack_damage;
+    }
+
+    public int getAttackSpeed() {
+        return this.attack_speed;
+    }
+
+    public int getRange() {
+        return this.range;
+    }
+
+    public int getCooldown() {
+        return this.cooldown;
+    }
 }
--- src/zombie/Zombie.java
+++ src/zombie/Zombie.java
@@ -1,5 +1,37 @@
 package zombie;
 
-public class Zombie {
-    
+public abstract class Zombie {
+    private String name;
+    private int health;
+    private int attack_damage;
+    private int attack_speed;
+    private boolean is_aquatic;
+
+    public Zombie(String name, int health, int attack_damage, int attack_speed, boolean is_aquatic) {
+        this.name = name;
+        this.health = health;
+        this.attack_damage = attack_damage;
+        this.attack_speed = attack_speed;
+        this.is_aquatic = is_aquatic;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getAttackDamage() {
+        return this.attack_damage;
+    }
+
+    public int getAttackSpeed() {
+        return this.attack_speed;
+    }
+
+    public boolean getIsAquatic() {
+        return this.is_aquatic;
+    }
 }
