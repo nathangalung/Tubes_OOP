6a1d619491e86b2093df767fac8c8df9f28f1f49
Regan Adiesta Mahendra <reganadiestamahendra@gmail.com>
2024-04-30 22:19:21 +0700

    add sunflower + sun + duckytube z + method di zombie&plant

---
+++ src/zombie/DuckyTube.java
@@ -0,0 +1,9 @@
+package zombie;
+
+public class DuckyTube extends Zombie{
+    
+    
+    public DuckyTube(String name, int health, int attack_damage, int attack_speed, boolean is_aquatic){
+        super(name,health,attack_damage,attack_speed,is_aquatic);
+    }
+}
