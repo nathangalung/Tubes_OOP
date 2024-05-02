638e0a31a221e3e4e74c5e1d23abb8f16b39500b
Regan Adiesta Mahendra <reganadiestamahendra@gmail.com>
2024-05-02 20:46:30 +0700

    update random

---
+++ src/entities/plants/BulletGenerator.java
@@ -0,0 +1,44 @@
+package entities.plants;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import mains.GamePanel;
+
+public class BulletGenerator {
+    private List<Bullet> bullets = new ArrayList<Bullet>();
+
+    Bullet tempBullet;
+    GamePanel gamePanel;
+
+    public BulletGenerator(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+        addBullet(new Bullet(100, 100, gamePanel));
+
+
+    }
+
+    public void tick(){
+        for(int i = 0; i < bullets.size() ; i++){
+            tempBullet = bullets.get(i);
+            tempBullet.tick();
+        }
+    }
+
+    public void render(Graphics g){
+        for(int i = 0; i < bullets.size() ; i++){
+            tempBullet = bullets.get(i);
+            tempBullet.render(g);
+        }        
+    }
+
+    public void addBullet(Bullet b){
+        bullets.add(b);
+    }
+
+    public void removeBullet(Bullet b){
+        bullets.remove(b);
+    }
+    
+}
