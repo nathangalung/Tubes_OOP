a50ec9c63c75388aa078c19e03a31eb97b818207
khairunnize <118610372+khairunnize@users.noreply.github.com>
2024-05-02 11:08:14 +0700

    constructor

--- src/plant/Peashooter.java
+++ src/plant/Peashooter.java
@@ -1,7 +1,17 @@
 package plant;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Peashooter extends Plant {
+    private boolean isReadyToAttack;
+    private ScheduledExecutorService scheduler;
+
     public Peashooter(String name, int cost, int health, int attack_damage, int attack_speed, int range, int cooldown) {
-        super(name, cost, health, attack_damage, attack_speed, range, cooldown);
+        super("Peashooter", 100, 100, 25, 4, -1, 10);
+        this.isReadyToAttack = true; // set awal ready to attack
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
     } 
 }
